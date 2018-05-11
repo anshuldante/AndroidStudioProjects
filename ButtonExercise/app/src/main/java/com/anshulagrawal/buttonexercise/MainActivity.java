ab4d8d5d6c66c56d247cde7088d99eaa5aaa7c8b
Anshul Agrawal <Anshul.Agrawal@walmartlabs.com>
2018-05-11 20:43:19 +0530

    button exercise

--- ButtonExercise/app/src/main/java/com/anshulagrawal/buttonexercise/MainActivity.java
+++ ButtonExercise/app/src/main/java/com/anshulagrawal/buttonexercise/MainActivity.java
@@ -1,21 +1,19 @@
-package com.anshulagrawal.demoapp;
+package com.anshulagrawal.buttonexercise;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-import java.util.logging.Logger;
-
 public class MainActivity extends AppCompatActivity {
-
-    public void clickFunction(View view) {
-        Log.i("Info", "Button pressed!");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view){
+        Log.i("View","Button added!");
     }
 }
