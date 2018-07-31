7e48ef3291ad646010d152a55c24283b2d88aa64
Anshul Agrawal <Anshul.Agrawal@walmartlabs.com>
2018-07-31 07:35:03 +0530

    uber first cut

---
+++ Uber/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("6ac00f223cf6ffe7f0b7fd7c54a253140b896d86")
+                .clientKey("6eba7ad9a361c8e5171434bca601b9e4967a928e")
+                .server("http://ec2-13-58-23-129.us-east-2.compute.amazonaws.com/parse/")
+                .build()
+        );
+
+
+
+
+
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
