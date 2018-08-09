38fd3ed3c17090d864ffc989572c846a12fef87e
jamiahuswalton <jamiahuswalton@gmail.com>
2018-08-09 13:19:14 -0500

    Just added a button to press. This will start the alert.

--- AlertDemo/app/src/main/java/com/anshulagrawal/alertdemo/MainActivity.java
+++ AlertDemo/app/src/main/java/com/anshulagrawal/alertdemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonGetAlert = findViewById(R.id.button_getAlert);
+        buttonGetAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartAlert();
+            }
+        });
+    }
+
+    private void StartAlert() {
         new AlertDialog.Builder(this)
                 .setIcon(R.drawable.ic_launcher_background)
                 .setTitle("Are you sure?")
